59f8b984a239cd3e70fab411ebc9995501ada183
paninapress <NPacifico25@gmail.com>
2014-03-10 14:06:03 -0700

    class work

--- app/simple_controller.js
+++ app/simple_controller.js
@@ -9,5 +9,10 @@ wdi.SimpleController = function($scope) {
   var minimum = 1000;
   $scope.my_balance = 1221.78;
   $scope.hasSufficientFunds = $scope.my_balance > minimum;
+
+  $scope.remove = function(leName){
+    var index = $scope.students.indexOf(leName);
+    $scope.students.splice(index, 1);
+  };
 };
 
